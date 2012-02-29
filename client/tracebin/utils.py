1476a6e3d7addada56025f9256c2781d45489beb
Alex Gaynor <alex.gaynor@gmail.com>
2012-02-29 11:27:11 -0500

    very useful comment

--- client/tracebin/utils.py
+++ client/tracebin/utils.py
@@ -2,5 +2,7 @@ import struct
 
 
 def read_unpack(fmt, file):
+    # This is different from struct.unpack_from because it advances the
+    # position in the file
     size = struct.calcsize(fmt)
     return struct.unpack(fmt, file.read(size))
